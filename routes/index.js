d4701b6cc89159b2798379e50abe17154a3852dc
dunstad <anthony00.blount00@gmail.com>
2016-10-29 00:56:04 +0900

    Combined command interface and map viewer into one page

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var commands = [];
-var map = [];
-
-// render posted map data
 router.get('/map', function(req, res, next) {
-  res.render('map', {map: map});
-});
-
-// add more data to the current map
-router.post('/map', function(req, res, next) {
-  map = (JSON.parse(req.body.map));
-  res.sendStatus(200);
+  res.render('map');
 });
 
 // main page where you enter commands
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
-// respond with currently pending commands
-router.get('/commands', function(req, res, next) {
-  res.send(commands);
-  commands = [];
-});
-
-// add new pending command
-router.post('/commands', function(req, res, next) {
-  commands.push(req.body.command);
-  res.sendStatus(200);
-});
-
 
 module.exports = router;
