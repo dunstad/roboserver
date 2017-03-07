bffe284a9dc6fe1086fae953e946d82f2051dbdb
dunstad <anthony00.blount00@gmail.com>
2017-03-07 11:18:21 +0900

    reorganizing scanDirection to make different sized scans easier to perform

--- lua/scanDirection.lua
+++ lua/scanDirection.lua
@@ -10,32 +10,54 @@ function M.makeScanner(x, z, w, d)
 end
 
 -- functions to scan one row at the end of an axis
-local scanZPos = M.makeScanner(-32, 32, 64, 1);
-local scanZNeg = M.makeScanner(-32, -32, 64, 1);
-local scanXPos = M.makeScanner(32, -32, 1, 64);
-local scanXNeg = M.makeScanner(-32, -32, 1, 64);
+local scanZPosBig = M.makeScanner(-32, 32, 64, 1);
+local scanZNegBig = M.makeScanner(-32, -32, 64, 1);
+local scanXPosBig = M.makeScanner(32, -32, 1, 64);
+local scanXNegBig = M.makeScanner(-32, -32, 1, 64);
 
 local scanForwardMap = {
-  [0]=scanZPos,
-  [1]=scanXPos,
-  [2]=scanZNeg,
-  [3]=scanXNeg
+  [0]=0,
+  [1]=2,
+  [2]=1,
+  [3]=3
 };
 
 local scanBackMap = {
-  [0]=scanZNeg,
-  [1]=scanXNeg,
-  [2]=scanZPos,
-  [3]=scanXPos
+  [0]=1,
+  [1]=3,
+  [2]=0,
+  [3]=2
 };
 
+local scanBigMap = {
+  [0]=scanZPosBig,
+  [1]=scanZNegBig,
+  [2]=scanXPosBig,
+  [3]=scanXNegBig
+};
+
+
+-- local scanForwardBigMap = {
+--   [0]=scanZPosBig,
+--   [1]=scanXPosBig,
+--   [2]=scanZNegBig,
+--   [3]=scanXNegBig
+-- };
+--
+-- local scanBackBigMap = {
+--   [0]=scanZNegBig,
+--   [1]=scanXNegBig,
+--   [2]=scanZPosBig,
+--   [3]=scanXPosBig
+-- };
+
 -- orientation is from trackOrientation.lua
-function M.forward(y, times)
-  return scanForwardMap[orient.get()](y, times);
+function M.forwardBig(y, times)
+  return scanBigMap[scanForwardMap[orient.get()]](y, times);
 end
 
-function M.back(y, times)
-  return scanBackMap[orient.get()](y, times);
+function M.backBig(y, times)
+  return scanBigMap[scanBackMap[orient.get()]](y, times);
 end
 
 return M;
