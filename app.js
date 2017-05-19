931aff43030af24f4845bdedd5b315fa65fbd710
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-05-19 11:38:01 +0900

    added icons

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.set('db', db);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 
 // i really doubt i need all three of these, fix later
