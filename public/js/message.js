11faac26e0c3092b2e93d5edfabbfcaeab7dd398
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-06-28 16:48:00 +0900

    made the message banner load in a prettier way

--- public/js/message.js
+++ public/js/message.js
@@ -5,6 +5,9 @@ function main() {
   ];
   var randomMessage = messages[Math.floor(Math.random() * messages.length)];
   bannerMessage.innerHTML = randomMessage;
+
+  var bannerMessageDiv = document.getElementById('bannerMessageDiv');
+  bannerMessageDiv.classList.remove('hidden');
 }
 
 main();
