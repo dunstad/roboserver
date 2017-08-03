e24a797bbefbcf64b10884afbc46d64c87514fd6
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-08-03 11:25:05 +0900

    have test client use external test data

--- public/js/server/testClient.js
+++ public/js/server/testClient.js
@@ -4,154 +4,10 @@ var net = require('net');
 var port = 3001;
 var host = '127.0.0.1';
 
-var testInventory1 = {
-	'size': 64,
-	'side': -1,
-	'selected': 1
-};
-
-var testInventory1Slots = [
-	{
-		side: -1,
-		slotNum: 1,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Dirt",
-			'maxDamage': 0,
-			'maxSize': 64,
-			'name': "minecraft:dirt",
-			'size': 64
-		}
-	},
-	{
-		side: -1,
-		slotNum: 2,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Dirt",
-			'maxDamage': 0,
-			'maxSize': 64,
-			'name': "minecraft:dirt",
-			'size': 37
-		}
-	},
-	{
-		side: -1,
-		slotNum: 5,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Stone",
-			'maxDamage': 0,
-			'maxSize': 64,
-			'name': "minecraft:stone",
-			'size': 3
-		}
-	},
-	{
-		side: -1,
-		slotNum: 7,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Wooden Sword?",
-			'maxDamage': 100,
-			'maxSize': 1,
-			'name': "minecraft:wooden_sword?",
-			'size': 1
-		}
-	}
-];
-
-var testInventory2 = {
-	'size': 27,
-	'side': 3
-};
-
-var testInventory2Slots = [
-	{
-		side: 3,
-		slotNum: 1,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Dirt",
-			'maxDamage': 0,
-			'maxSize': 64,
-			'name': "minecraft:dirt",
-			'size': 4
-		}
-	},
-	{
-		side: 3,
-		slotNum: 2,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Dirt",
-			'maxDamage': 0,
-			'maxSize': 64,
-			'name': "minecraft:dirt",
-			'size': 7
-		}
-	},
-	{
-		side: 3,
-		slotNum: 5,
-		contents: {
-			'damage': 0,
-			'hasTag': false,
-			'label': "Stone",
-			'maxDamage': 0,
-			'maxSize': 64,
-			'name': "minecraft:stone",
-			'size': 25
-		}
-	}
-];
-
-var testScan = {
-	x: 0,
-	z: 0,
-	y: 0,
-	w: 3,
-	d: 3,
-	data: {
-		1: 1,
-		2: 1,
-		3: 1,
-		4: 1,
-		5: 1,
-		6: 1,
-		7: 1,
-		8: 1,
-		9: 1,
-		10: 1,
-		11: 1,
-		12: 1,
-		13: 1,
-		14: 0,
-		15: 1,
-		16: 1,
-		17: 1,
-		18: 1,
-		19: 1,
-		20: 1,
-		21: 1,
-		22: 1,
-		23: 0,
-		24: 1,
-		25: 1,
-		26: 1,
-		27: 1,
-		n: 27
-	}
-};
+let testData = require('testData');
 
 // let's add some noise to testScan
-var mapData = testScan.data;
+var mapData = testData.scan.data;
 for (var key in mapData) {
 	if (mapData[key] == 1) {
 		mapData[key] = Math.random() * 6;
@@ -163,7 +19,6 @@ var power = 1;
 
 function send(key, value) {
 	
-	// const writeBufferLength = 99999;
 	const writeBufferLength = 20;
 
 	let data = {};
@@ -214,14 +69,14 @@ client.on('data', (rawMessages)=>{
 			
 			if (data.command == 'for i=-2,5 do sendScan.volume(-3, -3, i, 8, 8, 1); end return true;') {
 				console.log('sending map!');
-				sendWithCost('map data', testScan);
+				sendWithCost('map data', testData.scan);
 			}
 			else if (data.command == 'return int.sendInventoryData(-1);') {
 				console.log('sending inventory!');
-				sendWithCost('inventory data', testInventory1);
-				for (slot of testInventory1Slots) {sendWithCost('slot data', slot);}
-				sendWithCost('inventory data', testInventory2);
-				for (slot of testInventory2Slots) {sendWithCost('slot data', slot);}
+				sendWithCost('inventory data', testData.internalInventory.meta);
+				for (slot of testData.internalInventory.slots) {sendWithCost('slot data', slot);}
+				sendWithCost('inventory data', testData.externalInventory.meta);
+				for (slot of testData.externalInventory.slots) {sendWithCost('slot data', slot);}
 			}
 			else if (data.command == 'return pos.sendLocation();') {
 				console.log('sending location')
---
+++ public/js/server/testData.js
@@ -0,0 +1,151 @@
+module.exports = {
+
+  internalInventory: {
+    meta: {
+      'size': 64,
+      'side': -1,
+      'selected': 1
+    },
+    slots: [
+      {
+        side: -1,
+        slotNum: 1,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Dirt",
+          'maxDamage': 0,
+          'maxSize': 64,
+          'name': "minecraft:dirt",
+          'size': 64
+        }
+      },
+      {
+        side: -1,
+        slotNum: 2,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Dirt",
+          'maxDamage': 0,
+          'maxSize': 64,
+          'name': "minecraft:dirt",
+          'size': 37
+        }
+      },
+      {
+        side: -1,
+        slotNum: 5,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Stone",
+          'maxDamage': 0,
+          'maxSize': 64,
+          'name': "minecraft:stone",
+          'size': 3
+        }
+      },
+      {
+        side: -1,
+        slotNum: 7,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Wooden Sword?",
+          'maxDamage': 100,
+          'maxSize': 1,
+          'name': "minecraft:wooden_sword?",
+          'size': 1
+        }
+      }
+    ]
+  },
+
+  externalInventory: {
+    meta: {
+      'size': 27,
+      'side': 3
+    },
+    slots: [
+      {
+        side: 3,
+        slotNum: 1,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Dirt",
+          'maxDamage': 0,
+          'maxSize': 64,
+          'name': "minecraft:dirt",
+          'size': 4
+        }
+      },
+      {
+        side: 3,
+        slotNum: 2,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Dirt",
+          'maxDamage': 0,
+          'maxSize': 64,
+          'name': "minecraft:dirt",
+          'size': 7
+        }
+      },
+      {
+        side: 3,
+        slotNum: 5,
+        contents: {
+          'damage': 0,
+          'hasTag': false,
+          'label': "Stone",
+          'maxDamage': 0,
+          'maxSize': 64,
+          'name': "minecraft:stone",
+          'size': 25
+        }
+      }
+    ]
+  },
+
+  scan: {
+    x: 0,
+    z: 0,
+    y: 0,
+    w: 3,
+    d: 3,
+    data: {
+      1: 1,
+      2: 1,
+      3: 1,
+      4: 1,
+      5: 1,
+      6: 1,
+      7: 1,
+      8: 1,
+      9: 1,
+      10: 1,
+      11: 1,
+      12: 1,
+      13: 1,
+      14: 0,
+      15: 1,
+      16: 1,
+      17: 1,
+      18: 1,
+      19: 1,
+      20: 1,
+      21: 1,
+      22: 1,
+      23: 0,
+      24: 1,
+      25: 1,
+      26: 1,
+      27: 1,
+      n: 27
+    }
+  }
+  
+};
