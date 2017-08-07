5684d2726dbb48ee6c573200d573f97cc0b7d8bb
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-08-07 16:56:30 +0900

    getting rid of junk

--- public/js/server/testClient.js
+++ public/js/server/testClient.js
@@ -1,42 +1,5 @@
 const net = require('net');
 
-// client.on('data', (rawMessages)=>{
-// 	console.log('Received: ' + rawMessages);
-// 	messages = String(rawMessages).split('\r\n').filter(s=>s).map(JSON.parse);
-// 	for (var data of messages) {
-// 		if (data.command) {
-			
-// 			if (data.command == 'for i=-2,5 do sendScan.volume(-3, -3, i, 8, 8, 1); end return true;') {
-// 				console.log('sending map!');
-// 				sendWithCost('map data', testData.scan);
-// 			}
-// 			else if (data.command == 'return int.sendInventoryData(-1);') {
-// 				console.log('sending inventory!');
-// 				sendWithCost('inventory data', testData.internalInventory.meta);
-// 				for (slot of testData.internalInventory.slots) {sendWithCost('slot data', slot);}
-// 				sendWithCost('inventory data', testData.externalInventory.meta);
-// 				for (slot of testData.externalInventory.slots) {sendWithCost('slot data', slot);}
-// 			}
-// 			else if (data.command == 'return pos.sendLocation();') {
-// 				console.log('sending location')
-// 				sendWithCost('robot position', pos);
-// 			}
-// 			else if (data.command == "local conf = require('config'); require('tcp').write({['available components']=conf.get(conf.path).components});") {
-// 				var components = process.argv[7] ? {raw:true} : {};
-// 				sendWithCost('available components', components);
-// 			}
-// 			else {
-// 				console.log('responding to command: ' + data.command);
-// 				sendWithCost('command result', [true, 'received command: ' + data.command]);
-// 			}
-
-// 		}
-// 		else if (data.message) {
-// 			console.log(data.message);
-// 		}
-// 	}
-// });
-
 class testClient {
 
 	constructor(testData) {
