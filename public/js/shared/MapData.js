42ab42082afe2c21dd27bf68b6a746199bcf8a20
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-08-15 14:09:00 +0900

    reading data from geolyzer scans might work now

--- public/js/shared/MapData.js
+++ public/js/shared/MapData.js
@@ -29,6 +29,8 @@ class MapData {
 
   /**
    * Store block data in or remove it from the map.
+   * This will only change properties listed in blockData.
+   * If blockData is falsy, it removes the entry.
    * @param {number} x
    * @param {number} y
    * @param {number} z
@@ -38,7 +40,12 @@ class MapData {
   set(x, y, z, blockData) {
     if (!this.map[x]) {this.map[x] = {};}
     if (!this.map[x][y]) {this.map[x][y] = {};}
-    this.map[x][y][z] = blockData;
+    if (blockData) {
+      Object.assign(this.map[x][y][z], blockData);
+    }
+    else {
+      this.map[x][y][z] = undefined;
+    }
     return blockData;
   }
 
@@ -48,7 +55,23 @@ class MapData {
    */
   setFromGeolyzerScan(geolyzerScan) {
     validators.geolyzerScan(geolyzerScan);
-    
+    for (let x = 0; x < geolyzerScan.w; x++) {
+      for (let z = 0; z < geolyzerScan.d; z++) {
+        for (let y = 0; y < (geolyzerScan.data.n / (geolyzerScan.w * geolyzerScan.d)); y++) {
+  
+          let xWithOffset = x + geolyzerScan.x;
+          let yWithOffset = y + geolyzerScan.y;
+          let zWithOffset = z + geolyzerScan.z;
+  
+          // this is how the geolyzer reports 3d data in a 1d array
+          // also lua is indexed from 1
+          let index = (x + 1) + z*geolyzerScan.w + y*geolyzerScan.w*geolyzerScan.d;
+          
+          this.set(xWithOffset, yWithOffset, zWithOffset, geolyzerScan.data[index]);
+  
+        }
+      }
+    }
   }
 
 }
