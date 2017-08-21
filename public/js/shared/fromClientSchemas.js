82ebe95829cb8e0eca5c0b6d47b0e46b9d38e837
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-08-21 13:36:04 +0900

    client schema file added

---
+++ public/js/shared/fromClientSchemas.js
@@ -0,0 +1,13 @@
+if (!Ajv) {var Ajv = require("ajv")};
+const ajv = Ajv({allErrors: true, $data: true});
+
+const validators = {
+
+  
+
+};
+
+try {
+  module.exports = validators;
+}
+catch (e) {;}
