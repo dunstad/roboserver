701722bb68c5735a466f37123a85ee41cb65258f
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-09-06 13:46:17 +0900

    comment removed

--- public/js/server/robotTest.js
+++ public/js/server/robotTest.js
@@ -95,7 +95,6 @@ let tests = {
 
     testClient.equip();
 
-    // not sure if this comparison works well enough here
     assert(testClient.equipped == selectedSlotStack);
     assert(!testInventory.slots[selectedIndex]);
 
@@ -103,7 +102,7 @@ let tests = {
 
   testGetBoxPoints: (testClient)=>{
 
-
+    
 
   },
 
