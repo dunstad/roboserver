2ff4867d2dc790ee01be92b1093ed3addc680174
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-09-29 15:12:18 +0900

    starting to feel like i really need a GUI class first

---
+++ public/js/client/WebClient.js
@@ -0,0 +1,86 @@
+class WebClient {
+
+  constructor() {
+
+    this.commandMap = {
+
+      "message": console.dir,
+
+      "command result": ()=>{
+
+        
+
+      },
+
+      "map data": ()=>{
+
+        
+
+      },
+
+      "block data": ()=>{
+
+        
+
+      },
+
+      "robot position": ()=>{
+
+        
+
+      },
+
+      "delete selection": ()=>{
+
+        
+
+      },
+
+      "dig success": ()=>{
+
+        
+
+      },
+
+      "inventory data": ()=>{
+
+        
+
+      },
+
+      "slot data": ()=>{
+
+        
+
+      },
+
+      "listen start": ()=>{
+
+        
+
+      },
+
+      "listen end": ()=>{
+
+        
+
+      },
+
+      "power level": ()=>{
+
+        
+
+      },
+
+      "available components": ()=>{
+
+        
+
+      },
+
+
+    };
+
+  }
+
+}
