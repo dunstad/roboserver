42f068221d5f00f30dd19da99d5738ec2e480dc0
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-09-29 11:50:22 +0900

    made command schemas easier to make

--- public/js/shared/fromClientSchemas.js
+++ public/js/shared/fromClientSchemas.js
@@ -1,44 +1,61 @@
 if (!Ajv) {var Ajv = require("ajv")};
 const ajv = Ajv({allErrors: true, $data: true});
 
+/**
+ * 
+ * @param {object} ajv 
+ * @param {object} innerSchema 
+ * @param {string} id
+ * @return {object}
+ */
+function makeCommandValidator(ajv, innerSchema, id, validators) {
 
-function makeCommandValidator() {
+  ajv.addSchema(innerSchema, id);
 
-}
-
-let scanArea = {
-  "properties": {
-    "name": {
-      "type": "string",
-      "pattern": "^scanArea$",
-    },
-    "parameters": {
-      "type": "array",
-      "items": [{ "type": "integer"}],
-      "additionalItems": false,
-      "minItems": 1,
-      "maxItems": 1,
-    },
-  },
-  "additionalProperties": false,
-  "required": ["name", "parameters"],
-}
-
-ajv.addSchema(scanArea, 'scanArea');
-
-const validators = {
-
-  scanArea: ajv.compile({
+  let schema = {
     "properties": {
-      "command": {"$ref": "scanArea#/"},
+      "command": {"$ref": id},
       "robot": {"type": "string"},
     },
     "additionalProperties": false,
     "required": ["command", "robot"],
-  }),
+  };
 
-};
+  let result = ajv.compile(schema);
 
+  validators[id] = result;
+
+  return result;
+
+}
+
+const commandSchemas = {
+
+  scanArea: {
+    "properties": {
+      "name": {
+        "type": "string",
+        "pattern": "^scanArea$",
+      },
+      "parameters": {
+        "type": "array",
+        "items": [{ "type": "integer"}],
+        "additionalItems": false,
+        "minItems": 1,
+        "maxItems": 1,
+      },
+    },
+    "additionalProperties": false,
+    "required": ["name", "parameters"],
+  },
+  
+}
+
+const validators = {};
+
+for (let id in commandSchemas) {
+  makeCommandValidator(ajv, commandSchemas[id], id, validators);
+}
 try {
   module.exports = validators;
 }
