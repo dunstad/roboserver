3d715357add4ef2e3e12bd52a44bf985141edc95
Anthony Blount <anthonyblount@MacBook-Pro-2.local>
2017-10-05 17:26:35 +0900

    refactoring 4: legend of the refactor

--- public/js/client/WebClient.js
+++ public/js/client/WebClient.js
@@ -10,75 +10,91 @@ class WebClient {
 
     this.commandMap = {
 
-      "message": console.dir,
+      'message': console.dir,
 
-      "command result": ()=>{
+      /**
+       * Used to display command results received from robots.
+       * @param {object} result
+       */
+      'command result': (result)=>{
+        console.dir('command result');
+        console.dir(result);
+        this.addMessage(result.data, false);
+      },
+
+      'map data': (mapData)=>{
+        console.dir('map data');
+        console.dir(mapData);
+        this.game.mapRender.addShapeVoxels(mapData.data, mapData.robot);
+      },
+
+      'block data': (blockData)=>{
+        console.dir('block data');
+        console.dir(blockData);
+        let pos = new WorldAndScenePoint(blockData.data.point, true);
+        if (!(blockData.data.name == 'minecraft:air')) {
+          this.game.mapRender.addVoxel(pos, this.game.mapRender.colorFromHardness(blockData.data.hardness));
+        }
+        else {this.game.mapRender.removeVoxel(pos);}
+      },
+
+      'robot position': (pos)=>{
+        console.dir('robot position');
+        console.dir(pos);
+        this.game.mapRender.moveRobotVoxel(new WorldAndScenePoint(pos.data, true), pos.robot);
+        this.game.webClient.allRobotInfo[pos.robot].removeAllExternalInventories();
+        if (pos.robot == document.getElementById('robotSelect').value) {
+          let robotData = this.game.webClient.allRobotInfo[pos.robot];
+          if (robotData) {
+            this.game.mapRender.selectedRobotMesh.position.copy(robotData.getPosition().scene());
+            this.game.mapRender.requestRender();
+          }
+        }
+      },
+
+      'delete selection': ()=>{
 
         
 
       },
 
-      "map data": ()=>{
+      'dig success': ()=>{
 
         
 
       },
 
-      "block data": ()=>{
+      'inventory data': ()=>{
 
         
 
       },
 
-      "robot position": ()=>{
+      'slot data': ()=>{
 
         
 
       },
 
-      "delete selection": ()=>{
+      'listen start': ()=>{
 
         
 
       },
 
-      "dig success": ()=>{
+      'listen end': ()=>{
 
         
 
       },
 
-      "inventory data": ()=>{
+      'power level': ()=>{
 
         
 
       },
 
-      "slot data": ()=>{
-
-        
-
-      },
-
-      "listen start": ()=>{
-
-        
-
-      },
-
-      "listen end": ()=>{
-
-        
-
-      },
-
-      "power level": ()=>{
-
-        
-
-      },
-
-      "available components": ()=>{
+      'available components': ()=>{
 
         
 
