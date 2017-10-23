aa9ba911e29402d8d226a542a5bd59c3ac75c8ff
dunstad <anthony00.blount00@gmail.com>
2017-10-23 16:23:46 +0900

    adding some web client test data

--- public/js/server/webClientTestData.js
+++ public/js/server/webClientTestData.js
@@ -1,4 +1,14 @@
 module.exports = {
 
+  'listen start': {
+    robot: 'rob',
+  },
+  'listen end': {
+    robot: 'rob',
+  },
+  'command result': {
+    robot: 'rob',
+    parameters: [true, 'a test command result'],
+  }
   
 };
