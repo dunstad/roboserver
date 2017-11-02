162db65053281ee454955c2705b84019defd0245
dunstad <anthony00.blount00@gmail.com>
2017-11-02 16:27:38 +0900

    dig and place should accept separate coordinates now x1, y1, z1 instead of just a p1 object

--- public/lua/oc/doToArea.lua
+++ public/lua/oc/doToArea.lua
@@ -61,7 +61,9 @@ function M.makeApproachAndDoAction(action, scanType, times)
 end
 
 function M.makeDoActionToArea(action)
-  return function (p1, p2, index, scanType, times)
+  return function (x1, y1, z1, x2, y2, z2, index, scanType, times)
+    local p1 = {x: x1, y: y1, z: z1};
+    local p2 = {x: x2, y: y2, z: z2};
     local pointList = M.generateBoxPoints(p1, p2);
     adj.distanceSort(pos.get(), pointList);
     local approachAndDoAction = M.makeApproachAndDoAction(action, scanType, times);
