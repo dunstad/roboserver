b186701be768fafcfd14ae0afd58581086182a38
dunstad <anthony00.blount00@gmail.com>
2018-11-06 12:31:25 +0900

    apparently this environment doesn't include unpack
    this should fix the continuous reconnecting problems

--- public/lua/oc/commandLoop.lua
+++ public/lua/oc/commandLoop.lua
@@ -27,6 +27,13 @@ function runInTerminal(commandText)
   return output;
 end
 
+function unpack (t, i)
+  i = i or 1;
+  if t[i] ~= nil then
+    return t[i], unpack(t, i + 1);
+  end
+end
+
 -- wait until a command exists, grab it, execute it, and send result back
 function executeCommand()
   local data = tcp.read();
