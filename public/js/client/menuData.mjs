fc3aeb4f3d1b408490671523492537f64f319b29
dunstad <dunstad1@gmail.com>
2018-11-20 12:49:47 +0900

    menu looking good! fading might need to be quicker, not sure how that looks also might need to rearrange vectors in arrangements still

--- public/js/client/menuData.mjs
+++ public/js/client/menuData.mjs
@@ -1,14 +1,44 @@
+import { Menu } from '/js/client/Menu.mjs';
+
 // 'this' will be a Tile for these functions
 // because they get rebound, they can't be arrow functions
-export let menuData = {
+let menuData = {
   main: [
     {
       imageString: 'add',
-      onClick: function () {console.log('add!');},
+      onClick: function () {console.log('placeholder')},
     },
     {
       imageString: 'remove',
       onClick: function () {this.menu.fadeOut();},
     },
   ],
+  three: [
+    {
+      imageString: 'add',
+      onClick: function () {console.log('add!');},
+    },
+    {
+      imageString: '',
+      onClick: function () {},
+    },
+    {
+      imageString: 'remove',
+      onClick: function () {console.log('placeholder')},
+    },
+  ],
 };
+
+function makeMenuOpener(menuString) {
+  return function () {
+    this.menu.fadeOut();
+    let menuPos = this.menu.group.position;
+    let lookPos = this.menu.mapRender.controls.getObject().position;
+    new Menu(menuPos, lookPos, menuData[menuString], this.menu.mapRender);
+  };
+}
+
+menuData.main[0].onClick = makeMenuOpener('three');
+menuData.three[2].onClick = makeMenuOpener('main');
+
+export {menuData};
